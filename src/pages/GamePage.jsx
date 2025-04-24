904cd1596904b8a6d41b8f351746ed7f0a00454b
tobias <tobias.schmelzle@gmx.de>
2025-04-24 13:35:41 +0200

    added css files for pages

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import './GamePage.css';
 
 function GamePage() {
   const [status, setStatus] = useState("Warte auf Sprachbefehl...");
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+import './Homepage.css';
 
 function HomePage() {
   const navigate = useNavigate()
