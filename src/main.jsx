db10cab3076e5ba57672056b61541e7f073b377b
Tobias <tobias.schmelzle@gmx.de>
2025-04-27 22:30:01 +0200

    build optimizations; dynamically switch between HashRouter and BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, BrowserRouter } from 'react-router-dom';
 import App from "./App";
 
+const Router = window?.process?.versions?.electron ? HashRouter : BrowserRouter;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <Router>
     <App />
-  </HashRouter>
+  </Router>
 );
